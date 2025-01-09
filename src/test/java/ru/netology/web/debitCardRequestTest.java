8285806bfe048ffc5b7db8f86f7b4e6a6e88f3e3
natali.r <natasha-razdobreeva@mail.ru>
2025-01-09 09:41:19 +0300

    main3

--- src/test/java/ru/netology/web/debitCardRequestTest.java
+++ src/test/java/ru/netology/web/debitCardRequestTest.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class CallbackTest {
+class debitCardRequestTest {
     private WebDriver driver;
 
     @BeforeAll
